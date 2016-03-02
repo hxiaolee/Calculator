fc6edd86d47158d06413741f12ed32740ead671e
hxiaolee <hxiaolee@hxiaoleedemacbook-pro.local>
2016-03-02 23:19:36 +0800

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by hxiaolee on 16/3/2.
+//  Copyright © 2016年 hxiaolee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by hxiaolee on 16/3/2.
+//  Copyright © 2016年 hxiaolee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
